c47a2f715cb6cdcdb6e581a9fa2c8dded2ae159c
Pelayus <rodriguezalvarezpelayo30@gmail.com>
2024-12-23 10:02:40 +0100

    Commit inicial

---
+++ proyectoasturmatch/src/main/java/com/asturmatch/proyectoasturmatch/ProyectoasturmatchApplication.java
@@ -0,0 +1,13 @@
+package com.asturmatch.proyectoasturmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoasturmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoasturmatchApplication.class, args);
+	}
+
+}
---
+++ proyectoasturmatch/src/test/java/com/asturmatch/proyectoasturmatch/ProyectoasturmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asturmatch.proyectoasturmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoasturmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
