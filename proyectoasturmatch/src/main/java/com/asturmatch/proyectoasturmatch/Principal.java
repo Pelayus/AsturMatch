25e27f1409cfb1158ddc7b42bd42ee442e3c0de2
Pelayus <rodriguezalvarezpelayo30@gmail.com>
2024-12-23 10:43:01 +0100

    Ejecucion del programa con spring ✅ Conexion del programa con la BD ✅ Dependencias sin errores ✅

---
+++ proyectoasturmatch/src/main/java/com/asturmatch/proyectoasturmatch/Principal.java
@@ -0,0 +1,21 @@
+package com.asturmatch.proyectoasturmatch;
+
+import java.time.LocalDate;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Principal implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("\n\n\n\n\n\t\t\t\t\033[1;35m************************************************\033[0m");
+		System.out.println("\t\t\t\t\033[1;36mProyecto : Proyecto AsturMatch (Con Spring)\033[0m");
+		System.out.println("\t\t\t\t\033[1;33mAutor: Pelayo Rodríguez Álvarez\033[0m");
+		System.out.printf("\t\t\t\t\033[1;32mFecha: %-38s\033[0m\n", LocalDate.now().toString());
+		System.out.println("\t\t\t\t\033[1;35m************************************************\033[0m");
+		System.out.println("\n\t\t\t\033[1;36m-- Bienvenido/a a AsturMatch (Torneos Asturianos Personalizados) --\033[0m");
+	}
+
+}
--- proyectoasturmatch/src/main/java/com/asturmatch/proyectoasturmatch/ProyectoasturmatchApplication.java
+++ proyectoasturmatch/src/main/java/com/asturmatch/proyectoasturmatch/ProyectoasturmatchApplication.java
@@ -2,9 +2,15 @@ package com.asturmatch.proyectoasturmatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProyectoasturmatchApplication {
+	
+	@Bean
+	public Principal applicationStartupRunner(){
+		return new Principal();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoasturmatchApplication.class, args);
